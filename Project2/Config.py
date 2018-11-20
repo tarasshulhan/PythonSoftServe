d5cb4d2f6b7e8f4979a2508d8534a9edfe8114b1
tarasshulhan <taras.shulhan@gmail.com>
2018-11-20 23:55:17 +0200

    deleted useless files

--- Project2/Config.py
+++
@@ -1,6 +0,0 @@
-# Configurations
-# Enter Path Of Image Directories
-ImageDir = ["Pics"]
-
-# Image Extensions Allowed
-Extension = ['JPG','BMP','PNG']
--- Project2/ImgHandle.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageTk
-
-def tk_image(path):
- img = Image.open(path)
- width, height = img.size
- img = img.resize((width * 2, height * 2))
- storeobj = ImageTk.PhotoImage(img)
- return storeobj
